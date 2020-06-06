5141600582c3b70845a74798fe074d210f860bcd
Jagdev Singh Jhajj <jagdevsinghjhajj@Jagdevs-MacBook-Pro.local>
2020-06-05 20:30:38 -0400

    Setup Project

---
+++ Food.ML/Food.ML/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Food.ML
+//
+//  Created by Jagdev Singh Jhajj on 2020-06-05.
+//  Copyright © 2020 Jagdev Singh Jhajj. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
